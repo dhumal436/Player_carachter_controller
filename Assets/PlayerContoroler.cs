42022a3852b2f5060f1ab0b162ca334d0ae4da25
shubham dhumal <dhumalshubham436@gmail.com>
2018-08-09 19:42:54 +0530

    Initial commit

---
+++ Assets/PlayerContoroler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerContoroler : MonoBehaviour {
+    [SerializeField]
+    private float speed = 5f;
+    private PlayerMotor pm;
+    float lookSensitivity = 10f;
+    public void Start()
+    {
+        pm = GetComponent<PlayerMotor>();
+    }
+    public void Update()
+    {
+        float xmov = Input.GetAxisRaw("Horizontal");
+        float zmov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * xmov;
+        Vector3 _moveVertical = transform.forward * zmov;
+        //calcuklate Velocity
+        Vector3 velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+        pm.Move(velocity);
+        //calculate Rotation
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+            Vector3 rotation = new Vector3(0, _yRotation, 0) * lookSensitivity;
+           float Camrotation = _xRotation * lookSensitivity;
+        pm.Rotate(rotation, Camrotation);
+    
+    }
+
+}
---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+    public Vector3 velocity = Vector3.zero;
+    Vector3 _rot = Vector3.zero;
+    float _camrot = 0f;
+    float currentRotation = 0f;
+    float camLimit = 85f;
+    private Rigidbody rb;
+    [SerializeField]
+    private Camera cam;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(Mathf.Clamp(10, 1, 3));
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        PerformMovement();
+        PerformRotation();
+	}
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+        
+    }
+    public void Rotate(Vector3 rotation, float camRot) {
+        _rot=rotation;
+        _camrot = camRot;
+
+    } 
+
+
+    private void PerformMovement()
+    {
+        if(velocity!=null)
+        {
+            //MOveplayer
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        if ( cam!=null)
+        {
+            
+            currentRotation -= _camrot;
+            currentRotation = Mathf.Clamp(currentRotation,-camLimit,camLimit);
+            cam.transform.localEulerAngles=new Vector3(currentRotation, 0f, 0f);//limited Rotation 
+        }
+
+        //Rotate on x axis
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(_rot));
+    }
+
+}
